02df8c5c0909048c52a86f3f2887c3e50870a5ce
niejunhao <niejunhao@126.com>
2024-09-29 16:16:37 +0800

    Update diy-part3.sh

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -9,6 +9,20 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+sudo apt update -y
+
+sudo apt full-upgrade -y
+
+sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
+libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
+libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 \
+python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
+uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev bc lm-sensors pciutils luci-app-samba4 wsdd2 \
+libpam ddns-scripts wget-ssl luci-base luci libkmod luci-proto-ipv6 liblzma libpcre2 libnetsnmp \
+luci-compat miniupnpd conntrack conntrackd maccalc libcurl libtins libyaml-cpp glib2 libgpiod \
+libtirpc pciids libaio
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
@@ -114,4 +128,4 @@ sed -i '/mt7662u_rom_patch.bin/a\\techo mt76-usb disable_usb_sg=1 > $\(1\)\/etc\
 popd
 
 # Change default shell to zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+# sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
