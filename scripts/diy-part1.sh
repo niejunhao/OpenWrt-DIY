0a43e589d554c2faa994b06a6390bfa0fb7bd2ae
niejunhao <niejunhao@126.com>
2024-09-29 16:46:47 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -24,3 +24,20 @@ rm -rf ../lean/luci-theme-argon
 # Svn checkout packages from immortalwrt's repository
 git clone --depth=1 https://github.com/immortalwrt/packages -b openwrt-18.06 packages
 git clone --depth=1 https://github.com/immortalwrt/luci -b openwrt-18.06 luci
+
+sudo apt update -y
+
+sudo apt full-upgrade -y
+
+
+
+sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bison build-essential \
+bzip2 ccache cmake cpio curl device-tree-compiler fastjar flex gawk gettext gcc-multilib g++-multilib \
+git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libfuse-dev libglib2.0-dev libgmp3-dev \
+libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libpython3-dev libreadline-dev \
+libssl-dev libtool lrzsz mkisofs msmtp ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 \
+python3-pyelftools python3-setuptools qemu-utils rsync scons squashfs-tools subversion swig texinfo \
+uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev bc lm-sensors pciutils luci-app-samba4 wsdd2 \
+libpam ddns-scripts wget-ssl luci-base luci libkmod luci-proto-ipv6 liblzma libpcre2 libnetsnmp \
+luci-compat miniupnpd conntrack conntrackd maccalc libcurl libtins libyaml-cpp glib2 libgpiod \
+libtirpc pciids libaio
